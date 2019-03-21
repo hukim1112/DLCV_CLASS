e50d26f4364b12adc9ae18c369aeb19b15d134bb
dan <hyounguk1112@gmail.com>
2019-03-22 08:51:52 +0900

    update

--- download_datasets.sh
+++ download_datasets.sh
@@ -1,7 +1,8 @@
 export path=$1
 
 wget http://download.tensorflow.org/example_images/flower_photos.tgz -P $path
-wget http://download.tensorflow.org/models/inception_v1_2016_08_28.tar.gz -P $path
-
 tar -xf $path/flower_photos.tgz -C $path/
+wget http://download.tensorflow.org/models/inception_v1_2016_08_28.tar.gz -P $path
 tar -xf $path/inception_v1_2016_08_28.tar.gz -C $path/
+
+

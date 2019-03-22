a406dc2fdec8deaff2e4b8920538d902681781b1
dan <hyounguk1112@gmail.com>
2019-03-22 09:59:46 +0900

    update

--- download_datasets.sh
+++
@@ -1,8 +0,0 @@
-export path=$1
-
-wget http://download.tensorflow.org/example_images/flower_photos.tgz -P $path
-tar -xf $path/flower_photos.tgz -C $path/
-wget http://download.tensorflow.org/models/inception_v1_2016_08_28.tar.gz -P $path
-tar -xf $path/inception_v1_2016_08_28.tar.gz -C $path/
-
-
--- install_lib.sh
+++
@@ -1,2 +0,0 @@
-!pip install opencv-python==3.4.2.16
-!pip install opencv-contrib-python=3.4.2.16
